ee7795f99ba8807ce5b1469406c005f2c1b1f05c
JakubGV1 <sejfes@gmail.com>
2024-06-03 16:29:30 +0200

    new game

---
+++ XamarinV2/Activities/SequenceGameActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Bluetooth;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Views.InputMethods;
+using Android.Widget;
+using AndroidX.Activity;
+using AndroidX.Annotations;
+using AndroidX.AppCompat.App;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinV2.DTO;
+using XamarinV2.Entities;
+using XamarinV2.Enums;
+using GameState = XamarinV2.Enums.GameState;
+using System.Diagnostics;
+using Java.IO;
+using Java.Net;
+
+
+namespace XamarinV2
+{
+    [Activity(Label = "Sekwencja")]
+    public class SequenceGameActivity : AppCompatActivity
+    {
+
+
+    }
+
+}
